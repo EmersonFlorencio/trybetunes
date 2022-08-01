23ca18755630171062081600f2717c6270be9ed1
Emerson Florencio <emersonflorencio@hotmail.com>
2022-08-01 18:37:15 -0300

    Resolução do requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,13 +2,54 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameArtist: '',
+      disableBttn: true,
+    };
+  }
+
+  validButton = () => {
+    const { nameArtist } = this.state;
+    const min = 2;
+    if (nameArtist.length >= min) {
+      this.setState({ disableBttn: false });
+    } else {
+      this.setState({ disableBttn: true });
+    }
+  }
+
+  hendleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, () => { this.validButton(); });
+  }
+
   render() {
+    const { nameArtist, disableBttn } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>
-          Pagina de Procura!
-        </p>
+        <form>
+          <label htmlFor="login-artist-input">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              id="search-artist-input"
+              name="nameArtist"
+              value={ nameArtist }
+              onChange={ this.hendleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ disableBttn }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
