f9ba6f455581854fbdde4a9a73fa64873abc171c
Emerson Florencio <emersonflorencio@hotmail.com>
2023-05-21 12:52:46 -0300

    styles: finalizando o css da pagina profile e favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 
+import '../styles/styles-favorite.css';
+
 class Favorites extends React.Component {
   render() {
     return (
-      <div data-testid="page-favorites">
+      <div
+        data-testid="page-favorites"
+        className="favorite-container"
+      >
         <Header />
       </div>
     );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 
+import '../styles/styles-profile.css';
+
 class Profile extends React.Component {
   render() {
     return (
-      <div data-testid="page-profile">
+      <div
+        data-testid="page-profile"
+        className="profile-container"
+      >
         <Header />
       </div>
     );
